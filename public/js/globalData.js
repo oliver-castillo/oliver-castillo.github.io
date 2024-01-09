ee5f74c41bed02fe2fd8a05a4e27efac7c2a799c
Oliver Castillo <olcastillot@gmail.com>
2024-01-09 17:14:15 -0500

    Update files

---
+++ public/js/globalData.js
@@ -0,0 +1,9 @@
+const en = {
+  greeting: "Hey there!",
+  main_title: "I'm Oliver Castillo",
+  availability: "Open to work",
+  yearsExperience: "+1 year of experience.",
+  position: "Full-Stack Developer.",
+  place: "From Lima, Perú.",
+  purpose: "Focused on solving problems by applying new technologies."
+}
