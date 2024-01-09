44eed9df1c095ca84f1f0aee2a6db714e416dd42
Oliver Castillo <olcastillot@gmail.com>
2024-01-09 17:37:13 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://oliver-castillo.github.io',
-  base: '/oliver-castillo.github.io',
   integrations: [tailwind()]
 });
